7ba6c97cebdeb5ad6d89ff45bdbae2d57282c5d0
Sao Bran Aung <saobran.aung61@gmail.com>
2025-05-05 21:48:04 +0630

    feat: add delete user functionality with confirmation dialog and error handling

--- app/(main)/user/list/page.tsx
+++ app/(main)/user/list/page.tsx
@@ -1,19 +1,51 @@
 'use client';
 import { BasicTable } from '@/app/_components/Tables/BasicTable';
 import { PaginationType } from '@/app/_models/PaginationType';
-import { Get } from '@/app/_services/BasicHttpServices';
+import { Delete, Get } from '@/app/_services/BasicHttpServices';
 import Link from 'next/link';
+import { useRouter } from 'next/navigation';
 import React from 'react';
+import Swal from 'sweetalert2';
 
 type Props = {
     id: string;
 };
 
 const UserTableAction = ({ id }: Props) => {
+    const router = useRouter();
+
+    const DeleteUser = async (id: string) => {
+        // const response = await Delete('User', id);
+        Swal.fire({
+            title: 'Do you want to save the changes?',
+            showCancelButton: true,
+            confirmButtonText: 'Delete'
+        }).then((result) => {
+            /* Read more about isConfirmed, isDenied below */
+            if (result.isConfirmed) {
+                const deleteAction = async () => {
+                    const response = await Delete('User', id);
+                    Swal.fire('Deleted!', 'Data is successfully deleted', 'success');
+                    router.refresh();
+                };
+                deleteAction();
+            }
+        });
+    };
     return (
         <td>
-            <Link href={'/user/' + id} style={{ cursor: 'pointer' }}>
+            <Link href={`User/${id}`} style={{ cursor: 'pointer' }}>
                 Edit
+            </Link>
+            |
+            <Link
+                onClick={(e) => {
+                    e.preventDefault();
+                    DeleteUser(id);
+                }}
+                href={''}
+            >
+                Delete
             </Link>
         </td>
     );
--- app/_services/BasicHttpServices.ts
+++ app/_services/BasicHttpServices.ts
@@ -1,9 +1,17 @@
+import Swal from 'sweetalert2';
 import { PaginationType } from '../_models/PaginationType';
 import axiosInstance from './AxiosInstance';
 
 
 export const Get = async (url: string): Promise<PaginationType> => {
   const resp = await axiosInstance.get(url);
+  if (resp.status < 200 || resp.status >= 300) {
+    Swal.fire({
+      icon: "error",
+      title: "Oops...",
+      text: "Something went wrong!"
+    });
+  }
   const data = await resp.data;
   const responseData: PaginationType = JSON.parse(JSON.stringify(data));
   return responseData;
@@ -11,6 +19,13 @@ export const Get = async (url: string): Promise<PaginationType> => {
 
 export const GetSingle = async (url: string): Promise<any> => {
   const resp = await axiosInstance.get(url);
+  if (resp.status < 200 || resp.status >= 300) {
+    Swal.fire({
+      icon: "error",
+      title: "Oops...",
+      text: "Something went wrong!"
+    });
+  }
   const data = await resp.data;
   const responseData: PaginationType = JSON.parse(JSON.stringify(data));
   return responseData;
@@ -18,6 +33,13 @@ export const GetSingle = async (url: string): Promise<any> => {
 
 export const Post = async (url: string, data: unknown): Promise<any> => {
   const resp = await axiosInstance.post(url, data);
+  if (resp.status < 200 || resp.status >= 300) {
+    Swal.fire({
+      icon: "error",
+      title: "Oops...",
+      text: "Something went wrong!"
+    });
+  }
   const temp = await resp.data;
   const responseData: PaginationType = JSON.parse(JSON.stringify(temp));
   return responseData;
@@ -31,6 +53,13 @@ export const Put = async (
   const jsonObject = data as { [key: string]: unknown };
   jsonObject.id = id;
   const resp = await axiosInstance.put(url + '/' + id, jsonObject);
+  if (resp.status < 200 || resp.status >= 300) {
+    Swal.fire({
+      icon: "error",
+      title: "Oops...",
+      text: "Something went wrong!"
+    });
+  }
   const temp = await resp.data;
   const responseData: PaginationType = JSON.parse(JSON.stringify(temp));
   return responseData;
@@ -38,36 +67,21 @@ export const Put = async (
 
 export const Delete = async (url: string, id: string): Promise<any> => {
   const resp = await axiosInstance.delete(url + '/' + id);
+  if (resp.status < 200 || resp.status >= 300) {
+    Swal.fire({
+      icon:"error",
+      title:"Oops...",
+      text:"Something went wrong!"
+    })
+  }else{
+    Swal.fire({
+      icon:"success",
+      title:"Deleted",
+      text:'Successfully Deleted!'
+    }) 
+  }
   const temp = await resp.data;
   const responseData: PaginationType = JSON.parse(JSON.stringify(temp));
-  return responseData;
-};
-
-export const CheckExistingRegistration = async (): Promise<boolean> => {
-  const resp = await axiosInstance.get("Common/CheckExistingRegistration");
-  const data = await resp.data;
-  const responseData: boolean = JSON.parse(JSON.stringify(data));
-  return responseData;
-};
-
-export const GetIdForExtenstion = async (): Promise<string> => {
-  const resp = await axiosInstance.get("Common/GetIdForExtenstion");
-  const data = await resp.data;
-  const responseData: string = JSON.parse(JSON.stringify(data));
-  return responseData;
-};
-
-export const CheckValidDateForExtension = async (id: string): Promise<boolean> => {
-  const resp = await axiosInstance.get("Common/CheckValidDateForExtension?id=" + id);
-  const data = await resp.data;
-  const responseData: boolean = JSON.parse(JSON.stringify(data));
-  return responseData;
-};
-
-export const CheckValidDateForAmend = async (id: string): Promise<boolean> => {
-  const resp = await axiosInstance.get("Common/CheckValidDateForAmend?id=" + id);
-  const data = await resp.data;
-  const responseData: boolean = JSON.parse(JSON.stringify(data));
   return responseData;
 };
 
