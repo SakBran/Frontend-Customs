c2b6fe2374ae858dbf778dcf46f5b095f4073544
Sao Bran Aung <saobran.aung61@gmail.com>
2025-05-20 00:53:21 +0630

    feat: update code for dashboard api call and databinding

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,137 +1,70 @@
 /* eslint-disable @next/next/no-img-element */
 'use client';
 import { Button } from 'primereact/button';
-import { Chart } from 'primereact/chart';
 import { Column } from 'primereact/column';
 import { DataTable } from 'primereact/datatable';
-import { Menu } from 'primereact/menu';
-import React, { useContext, useEffect, useRef, useState } from 'react';
-import { ProductService } from '../../demo/service/ProductService';
-import { LayoutContext } from '../../layout/context/layoutcontext';
-import Link from 'next/link';
-import { Demo } from '@/types';
-import { ChartData, ChartOptions } from 'chart.js';
+import React, { useEffect, useState } from 'react';
+import axiosInstance from '../_services/AxiosInstance';
+import Swal from 'sweetalert2';
 
-const lineData: ChartData = {
-    labels: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturaday', 'Sunday'],
-    datasets: [
-        {
-            label: 'Sent Dataset',
-            data: [65, 59, 80, 81, 56, 55, 40],
-            fill: false,
-            backgroundColor: '#2f4860',
-            borderColor: '#2f4860',
-            tension: 0.4
-        },
-        {
-            label: 'Failed Dataset',
-            data: [28, 48, 40, 19, 86, 27, 90],
-            fill: false,
-            backgroundColor: '#00bb7e',
-            borderColor: '#00bb7e',
-            tension: 0.4
-        },
-        {
-            label: 'Not Sent Dataset',
-            data: [2, 2, 3, 0, 2, 1, 2],
-            fill: false,
-            backgroundColor: '#326fa8',
-            borderColor: '#326fa8',
-            tension: 0.4
-        },
-        {
-            label: 'Duplicate Dataset',
-            data: [10, 20, 30, 40, 50, 60, 70],
-            fill: false,
-            backgroundColor: '#a83232',
-            borderColor: '#a83232',
-            tension: 0.4
-        }
-    ]
-};
+interface GetDailyResponse {
+    sent: number;
+    notSent: number;
+    failed: number;
+    ceirid: number;
+    sentList: CustomsData[];
+    ceiridList: CeiridFromIrd[];
+}
+
+interface CustomsData {
+    id: string;
+    receivedDatetime?: string; // ISO format DateTime string
+    ceirid?: string;
+    oldCeirid?: string;
+    maccsCeirid?: string;
+    roNo?: string;
+    roDate?: string;
+    cd?: number;
+    ct?: number;
+    at?: number;
+    rf?: number;
+    sendBy?: string;
+    sendById?: string;
+    sentDatetime?: string;
+    remark?: string;
+    editBy?: string;
+    editById?: string;
+    editDatetime?: string;
+    status?: string;
+    editCeirid?: string;
+}
+
+interface CeiridFromIrd {
+    id: string;
+    ceirid: string;
+    receivedDatetime: string;
+    isSent: boolean;
+    sendDatetime?: string;
+}
 
 const Dashboard = () => {
-    const [products, setProducts] = useState<Demo.Product[]>([]);
-    const menu1 = useRef<Menu>(null);
-    const menu2 = useRef<Menu>(null);
-    const [lineOptions, setLineOptions] = useState<ChartOptions>({});
-    const { layoutConfig } = useContext(LayoutContext);
-
-    const applyLightTheme = () => {
-        const lineOptions: ChartOptions = {
-            plugins: {
-                legend: {
-                    labels: {
-                        color: '#495057'
-                    }
-                }
-            },
-            scales: {
-                x: {
-                    ticks: {
-                        color: '#495057'
-                    },
-                    grid: {
-                        color: '#ebedef'
-                    }
-                },
-                y: {
-                    ticks: {
-                        color: '#495057'
-                    },
-                    grid: {
-                        color: '#ebedef'
-                    }
-                }
-            }
-        };
-
-        setLineOptions(lineOptions);
-    };
-
-    const applyDarkTheme = () => {
-        const lineOptions = {
-            plugins: {
-                legend: {
-                    labels: {
-                        color: '#ebedef'
-                    }
-                }
-            },
-            scales: {
-                x: {
-                    ticks: {
-                        color: '#ebedef'
-                    },
-                    grid: {
-                        color: 'rgba(160, 167, 181, .3)'
-                    }
-                },
-                y: {
-                    ticks: {
-                        color: '#ebedef'
-                    },
-                    grid: {
-                        color: 'rgba(160, 167, 181, .3)'
-                    }
-                }
-            }
-        };
-
-        setLineOptions(lineOptions);
-    };
+    const [data, setData] = useState<GetDailyResponse>();
 
     useEffect(() => {
-        ProductService.getProductsSmall().then((data) => setProducts(data));
+        const dailyRequestAPI = async () => {
+            const resp = await axiosInstance.get('Dashboard');
+            if (resp.status < 200 || resp.status >= 300) {
+                Swal.fire({
+                    icon: 'error',
+                    title: 'Oops...',
+                    text: 'Something went wrong!'
+                });
+            }
+            const tempData = await resp.data;
+            setData(tempData);
+        };
+        dailyRequestAPI();
     }, []);
-
-    useEffect(() => {
-        if (layoutConfig.colorScheme === 'light') {
-            applyLightTheme();
-        } else {
-            applyDarkTheme();
-        }
-    }, [layoutConfig.colorScheme]);
 
     const formatCurrency = (value: number) => {
         return value?.toLocaleString('en-US', {
@@ -147,14 +80,14 @@ const Dashboard = () => {
                     <div className="flex justify-content-between mb-3">
                         <div>
                             <span className="block text-500 font-medium mb-3">Sent</span>
-                            <div className="text-900 font-medium text-xl">152</div>
+                            <div className="text-900 font-medium text-xl">{data?.sent}</div>
                         </div>
                         <div className="flex align-items-center justify-content-center bg-blue-100 border-round" style={{ width: '2.5rem', height: '2.5rem' }}>
                             <i className="pi pi-file-export text-blue-500 text-xl" />
                         </div>
                     </div>
-                    <span className="text-green-500 font-medium">24 new </span>
-                    <span className="text-500">since last visit</span>
+                    <span className="text-green-500 font-medium">Today </span>
+                    <span className="text-500">sent count</span>
                 </div>
             </div>
             <div className="col-12 lg:col-6 xl:col-3">
@@ -162,14 +95,14 @@ const Dashboard = () => {
                     <div className="flex justify-content-between mb-3">
                         <div>
                             <span className="block text-500 font-medium mb-3">Failed</span>
-                            <div className="text-900 font-medium text-xl">$2.100</div>
+                            <div className="text-900 font-medium text-xl">{data?.failed}</div>
                         </div>
                         <div className="flex align-items-center justify-content-center bg-orange-100 border-round" style={{ width: '2.5rem', height: '2.5rem' }}>
                             <i className="pi pi-ban text-orange-500 text-xl" />
                         </div>
                     </div>
-                    <span className="text-green-500 font-medium">%52+ </span>
-                    <span className="text-500">since last week</span>
+                    <span className="text-green-500 font-medium">Today </span>
+                    <span className="text-500">failed count</span>
                 </div>
             </div>
             <div className="col-12 lg:col-6 xl:col-3">
@@ -177,14 +110,14 @@ const Dashboard = () => {
                     <div className="flex justify-content-between mb-3">
                         <div>
                             <span className="block text-500 font-medium mb-3">Not Sent</span>
-                            <div className="text-900 font-medium text-xl">28441</div>
+                            <div className="text-900 font-medium text-xl">{data?.notSent}</div>
                         </div>
                         <div className="flex align-items-center justify-content-center bg-cyan-100 border-round" style={{ width: '2.5rem', height: '2.5rem' }}>
                             <i className="pi pi-wrench text-cyan-500 text-xl" />
                         </div>
                     </div>
-                    <span className="text-green-500 font-medium">520 </span>
-                    <span className="text-500">newly registered</span>
+                    <span className="text-green-500 font-medium">Today </span>
+                    <span className="text-500">not sent count</span>
                 </div>
             </div>
             <div className="col-12 lg:col-6 xl:col-3">
@@ -192,33 +125,24 @@ const Dashboard = () => {
                     <div className="flex justify-content-between mb-3">
                         <div>
                             <span className="block text-500 font-medium mb-3">CEIRID From IRD</span>
-                            <div className="text-900 font-medium text-xl">152 Unread</div>
+                            <div className="text-900 font-medium text-xl">{data?.ceirid}</div>
                         </div>
                         <div className="flex align-items-center justify-content-center bg-purple-100 border-round" style={{ width: '2.5rem', height: '2.5rem' }}>
                             <i className="pi pi-clone text-purple-500 text-xl" />
                         </div>
                     </div>
-                    <span className="text-green-500 font-medium">85 </span>
-                    <span className="text-500">responded</span>
+                    <span className="text-green-500 font-medium">Today </span>
+                    <span className="text-500">total count for CEIRID from IRD</span>
                 </div>
             </div>
 
             <div className="col-12 xl:col-6">
                 <div className="card">
                     <h5>Today Sent list</h5>
-                    <DataTable value={products} rows={5} paginator responsiveLayout="scroll">
-                        <Column header="Image" body={(data) => <img className="shadow-2" src={`/demo/images/product/${data.image}`} alt={data.image} width="50" />} />
-                        <Column field="name" header="Name" sortable style={{ width: '35%' }} />
-                        <Column field="price" header="Price" sortable style={{ width: '35%' }} body={(data) => formatCurrency(data.price)} />
-                        <Column
-                            header="View"
-                            style={{ width: '15%' }}
-                            body={() => (
-                                <>
-                                    <Button icon="pi pi-search" text />
-                                </>
-                            )}
-                        />
+                    <DataTable value={data?.sentList} rows={5} paginator responsiveLayout="scroll">
+                        <Column field="ceirid" header="CEIR ID" sortable style={{ width: '35%' }} />
+                        {/* <Column field="sentDatetime" header="Sent Datetime" sortable style={{ width: '35%' }} body={(data) => formatCurrency(data.price)} /> */}
+                        <Column field="sentDatetime" header="Sent Datetime" sortable style={{ width: '35%' }} />
                     </DataTable>
                 </div>
             </div>
@@ -226,19 +150,10 @@ const Dashboard = () => {
             <div className="col-12 xl:col-6">
                 <div className="card">
                     <h5>Today CEIRID Sent list from IRD</h5>
-                    <DataTable value={products} rows={5} paginator responsiveLayout="scroll">
-                        <Column header="Image" body={(data) => <img className="shadow-2" src={`/demo/images/product/${data.image}`} alt={data.image} width="50" />} />
-                        <Column field="name" header="Name" sortable style={{ width: '35%' }} />
-                        <Column field="price" header="Price" sortable style={{ width: '35%' }} body={(data) => formatCurrency(data.price)} />
-                        <Column
-                            header="View"
-                            style={{ width: '15%' }}
-                            body={() => (
-                                <>
-                                    <Button icon="pi pi-search" text />
-                                </>
-                            )}
-                        />
+                    <DataTable value={data?.ceiridList} rows={5} paginator responsiveLayout="scroll">
+                        <Column field="ceirid" header="CEIR ID" sortable style={{ width: '35%' }} />
+                        <Column field="receivedDatetime" header="Received Datetime" sortable style={{ width: '35%' }} />
+                        <Column field="isSent" header="Is Sent" sortable style={{ width: '30%' }} />
                     </DataTable>
                 </div>
             </div>
